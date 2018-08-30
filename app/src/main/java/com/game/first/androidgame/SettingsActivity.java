ee12d7111ef4d81bab5c193f7b73a9826cc57b0a
Serhii <serhiycoo@gmail.com>
2018-08-30 08:39:01 +0200

    Fixed invoking next activity in SettingsActivity.java

--- app/src/main/java/com/game/first/androidgame/SettingsActivity.java
+++ app/src/main/java/com/game/first/androidgame/SettingsActivity.java
@@ -15,21 +15,22 @@ public class SettingsActivity extends AppCompatActivity {
     private Button nextButton;
     private SeekBar timeRange;
     private int minRoundTime = 10;
+    private Game gameInstance;
 
 
-    protected void onCreate() {
-        //super.onCreate();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
         setTitle(R.string.settings_activity_title);
-
+        gameInstance = getIntent().getParcelableExtra(TeamsActivity.GAME_INSTANCE);
 
         nextButton = findViewById(R.id.settings_continue_button);
 
-        /*nextButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, TeamsActivity.class);
-            intent.putExtra(CREATE_NEW, true);
+        nextButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RatingsActivity.class);
+            intent.putExtra(TeamsActivity.GAME_INSTANCE, gameInstance);
             startActivity(intent);
-        });*/
+        });
 
 
     }
